dfd83d123e944d8fc8e7194c6bc62e7951d97e26
Ag <70638763+ag-0224@users.noreply.github.com>
2024-04-24 20:54:22 +0900

    [Gold V] Title: 탑, Time: 828 ms, Memory: 173676 KB -BaekjoonHub

---
+++ 백준/Gold/2493. 탑/탑.py
@@ -0,0 +1,26 @@
+n = int(input())
+
+stack = []
+height = list(map(int, input().split()))
+
+top_list = []
+
+for i in range(len(height)):
+    top_list.append({ 'height' : height[i], 'id' : i + 1})
+
+for top in top_list:
+    if not stack:
+        stack.append(top)
+        print(0, end=' ')
+    else:
+        while stack:
+            if stack[-1]['height'] >= top['height']:
+                print(stack[-1]['id'], end=' ')
+                stack.append(top)
+                break
+            else:
+                stack.pop()
+
+        if not stack:        
+            stack.append(top)
+            print(0, end=' ')
