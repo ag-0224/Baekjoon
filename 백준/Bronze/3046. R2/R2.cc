f448f91c978db77ab5c51f51ebd3462660e0224c
Ag <70638763+ag-0224@users.noreply.github.com>
2024-05-29 16:42:18 +0900

    [Bronze IV] Title: R2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/3046. R2/R2.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int r1, r2, s;
+
+int main(void) {
+	cin.tie(0)->sync_with_stdio(false);
+
+	cin >> r1 >> s;
+
+	cout << 2 * s - r1;
+
+	return 0;   
+}
