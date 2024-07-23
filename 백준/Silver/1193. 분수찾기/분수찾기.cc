95296406b86e4c4e113177aac15b674472dc7e00
Ag <70638763+ag-0224@users.noreply.github.com>
2024-07-23 19:35:15 +0900

    [Silver V] Title: 분수찾기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/1193. 분수찾기/분수찾기.cc
+++ 백준/Silver/1193. 분수찾기/분수찾기.cc
@@ -2,58 +2,24 @@
 
 using namespace std;
 
-int X;
+int x;
 
-int board[5000][5000];
-
-const int dy[4] = { 0, 1, 1, -1 };
-const int dx[4] = { 1, -1, 0, 1 };
-
-pair<int, int> go(int n) {
-	pair<int, int> ret;
-
-	int cnt = 1;
-
-	int y = 0;
-	int x = 0;
-
-	bool check = false;
-	while (cnt < n) {
-		if (y == 0 && !check) {
-			y = y + dy[0];
-			x = x + dx[0];
-			check = true;
-		}
-		else if (x == 0 && !check) {
-			y = y + dy[2];
-			x = x + dx[2];
-			check = true;
-		}
-		else if ((x + y) % 2 == 1) {
-			y = y + dy[1];
-			x = x + dx[1];
-			check = false;
-		}
-		else if ((x + y) % 2 == 0) {
-			y = y + dy[3];
-			x = x + dx[3];
-			check = false;
-		}
-
-		cnt++;
-	}  
-
-	ret = { y, x };
-
-	return ret;
-}
- 
 int main() {
-	cin >> X;
+	cin >> x;
 
-	auto ret = go(X);
+	int line = 1;
 
-	cout << ret.first + 1 << "/" << ret.second + 1;
+	while (x > line) {
+		x -= line;
+		line++;
+	}
+
+	if (line % 2 == 1) {
+		cout << line - x + 1 << "/" << x;
+	}
+	else {
+		cout << x << "/" << line - x + 1;
+	}
 
 	return 0;
 }
