74a910afd0897d99e3e55afe7d813820382ee3f2
Ag <70638763+ag-0224@users.noreply.github.com>
2024-07-24 19:05:25 +0900

    [Silver III] Title: 수열, Time: 8 ms, Memory: 2412 KB -BaekjoonHub

--- 백준/Silver/2559. 수열/수열.cc
+++ 백준/Silver/2559. 수열/수열.cc
@@ -2,24 +2,20 @@
 
 using namespace std;
 
-int a[100004], n, k, ret = - 10000000;
-int s[100004];
+int n, k, temp, psum[100001], ret = -1000000;
 
 int main() {
 	ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
 
 	cin >> n >> k;
 
-	for (int i = 0; i < n; i++) {
-		cin >> a[i];
-		s[i + 1] = s[i] + a[i];
+	for (int i = 1; i <= n; i++) {
+		cin >> temp;
+		psum[i] = psum[i - 1] + temp;
 	}
 
 	for (int i = k; i <= n; i++) {
-		int sum = 0;
-		sum = s[i] - s[i - k];
-
-		ret = max(ret, sum);
+		ret = max(ret, psum[i] - psum[i - k]);
 	}
 
 	cout << ret << "\n";
