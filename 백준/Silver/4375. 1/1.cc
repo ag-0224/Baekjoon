75a59b6bd975946c9b3eed62f9e69d5f6594c71d
Ag <70638763+ag-0224@users.noreply.github.com>
2024-07-26 17:58:19 +0900

    [Silver III] Title: 1, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/4375. 1/1.cc
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+
+ll n, ret;
+int cnt;
+
+int main() {
+	ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+
+	while (cin >> n) {
+		ret = 1;
+		cnt = 1;
+
+		while (ret % n != 0) {
+			ret = ret * 10 + 1;
+			ret %= n;
+			cnt++;
+		}
+
+		cout << cnt << "\n";
+	}
+
+	return 0;
+}
