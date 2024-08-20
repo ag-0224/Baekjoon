82eea7f0f1dee52f0c047cc3256ced080e6c0b0b
Ag <70638763+ag-0224@users.noreply.github.com>
2024-08-20 13:06:14 +0900

    [Bronze V] Title: 세금, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/20492. 세금/세금.cc
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int n;
+
+int main() {
+
+    cin >> n;
+
+    cout << fixed << setprecision(0) << n * 0.78;
+
+    cout << " "; 
+
+    cout << fixed << setprecision(0) << n * 0.956;
+
+    return 0;
+
+}
