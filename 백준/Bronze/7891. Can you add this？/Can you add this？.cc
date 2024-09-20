025c88bc05ab7421ff0f50d0831a69d547cf0490
Ag <70638763+ag-0224@users.noreply.github.com>
2024-09-20 22:50:18 +0900

    [Bronze V] Title: Can you add this?, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/7891. Can you add this？/Can you add this？.cc
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+
+int t;
+ll x, y;
+
+int main() {
+	ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+
+	cin >> t;
+
+	while (t--) {
+		cin >> x >> y;
+		cout << x + y << "\n";
+	}
+
+	return 0;
+}
