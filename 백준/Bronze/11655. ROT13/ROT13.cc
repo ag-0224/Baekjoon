0a9a21f3d747314c57bf1157c336fe6f81edd407
Ag <70638763+ag-0224@users.noreply.github.com>
2024-10-21 10:32:04 +0900

    [Bronze I] Title: ROT13, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Bronze/11655. ROT13/ROT13.cc
+++ 백준/Bronze/11655. ROT13/ROT13.cc
@@ -2,25 +2,26 @@
 
 using namespace std;
 
-string s;
-char ret;
+string s, ret;
 
 int main() {
-	ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
 
 	getline(cin, s);
 
-	for (int i = 0; i < s.length(); i++) {
-		if ('a' <= s[i] && s[i] <= 'z') {
-			ret = (s[i] + 13 - 'a') % 26 + 'a';
-			cout << ret;
+	for (char x : s) {
+		if ('a' <= x && x <= 'z') {
+			ret += (x - 'a' + 13) % 26 + 'a';
 		}
-		else if ('A' <= s[i] && s[i] <= 'Z') {
-			ret = (s[i] + 13 - 'A') % 26 + 'A';
-			cout << ret;
+		else if ('A' <= x && x <= 'Z') {
+			ret += (x - 'A' + 13) % 26 + 'A';
 		}
-		else cout << s[i];
+		else ret += x;
 	}
+
+	cout << ret;
 
 	return 0;
 }
